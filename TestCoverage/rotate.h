95de9b27d78f280629a121456f5fd64adfdcd313
stolas0 <tabaki98@yandex.ru>
2022-05-12 17:17:52 +0300

    Test coverage

---
+++ TestCoverage/rotate.h
@@ -0,0 +1,31 @@
+#pragma once
+
+template<class T>
+struct Vector {
+    T x;
+    T y;
+
+    Vector(T X, T Y): x(X), y(Y) {};
+
+    bool operator==(const Vector& v) const {
+        return x == v.x && y == v.y;
+    }
+};
+
+template<class T>
+Vector<T> rotate(const Vector<T>& v, int degree) {
+
+    while(degree < 0) degree += 360;
+    while(degree >= 360) degree -= 360;
+
+    if (degree == 90)
+        return Vector<T>(-v.y, v.x);
+
+    if (degree == 180)
+        return Vector<T>(-v.x, -v.y);
+
+    if (degree == 270)
+        return Vector<T>(v.y, -v.x);
+
+    return Vector<T>(v.x, v.y);
+}
---
+++ TestCoverage/rotate_test.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include "coverage/rotate.h"
+
+int main() {
+    Vector<int> vec(1, 2);
+
+    Vector<int> ans1(1, 2);
+    EXPECT_EQ(ans1, rotate(vec, -360));
+
+    Vector<int> ans2(1, 2);
+    EXPECT_EQ(ans2, rotate(vec, 360));
+    
+    Vector<int> ans3(-2, 1);
+    EXPECT_EQ(ans3, rotate(vec, 90));
+    
+    Vector<int> ans4(-1, -2);
+    EXPECT_EQ(ans4, rotate(vec, 180));
+    
+    Vector<int> ans5(2, -1);
+    EXPECT_EQ(ans5, rotate(vec, 270));
+
+    EXPECT_FALSE(Vector<int>(1, 1) == Vector<int>(1, 2));
+    EXPECT_FALSE(Vector<int>(1, 1) == Vector<int>(2, 1));
+
+    return 0;
+}
