365e46faa98401cd14135f21f5b886cf58c40dd6
stolas0 <tabaki98@yandex.ru>
2022-05-12 17:23:48 +0300

    My concatenation

---
+++ MyConcatenation/cat_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <array>
+#include <myproject/cat.h>
+
+TEST(A, B) {
+    array<int, 3> a{1, 2, 3};
+    array<int, 3> b{4, 5, 6};
+    array<int, 6> c{1, 2, 3, 4, 5 ,6};
+    EXPECT_EQ(c, cat(a, b));
+}
